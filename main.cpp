a5db8d4b83e2f4716e56fdfefa80de4872681ede
xzores <jakobfu@hotmail.com>
2024-03-25 12:34:27 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -73,10 +73,6 @@ int main (int argc, char **argv)
 	if (not service.theEnd) { 
 
 		gpio.setPin(16, 1);
-		mixer.setDesiredHeading(0);
-		mixer.setVelocity(0.2);
-		usleep(2*1000*1000);
-		/*
 		{
 			auto p = furbs.vel;
 			furbs.go_for(4.73, left_line_mode, 0, 0, p);
@@ -124,7 +120,6 @@ int main (int argc, char **argv)
 			p.max_vel += 0.2;
 			furbs.go_for(4, right_line_mode, 0.5, 0, p);
 		}
-		*/
 		gpio.setPin(16, 0);
 	
 	}
